645f479b0f7ba4376373a45c8f0d7904f0b95398
Vladimir Jimenez <allejo@me.com>
2017-11-29 18:22:53 -0800

    Add recaptcha to registration form

--- src/AppBundle/Form/RegistrationFormType.php
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -3,11 +3,13 @@
 namespace AppBundle\Form;
 
 use Carbon\Carbon;
+use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
+use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -20,6 +22,12 @@ class RegistrationFormType extends AbstractType
             ->add('lastName', TextType::class, [])
             ->add('birthday', BirthdayType::class, [
                 'years' => array_reverse(range(1900, $now->year - 13))
+            ])
+            ->add('recaptcha', EWZRecaptchaType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new RecaptchaTrue()
+                ]
             ])
         ;
     }
